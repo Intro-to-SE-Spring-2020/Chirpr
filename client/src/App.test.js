c1cfc637f71e13c093c1846b0b99aebc377fdefc
JustinASmith <justinasmith662@gmail.com>
2020-01-25 17:58:39 -0600

    Update test case for App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Containing component \/ Entry-point/i);
   expect(linkElement).toBeInTheDocument();
 });
