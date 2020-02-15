76df03057e4e3a0ec19a7411b59141bf89d91a2d
JustinASmith <justinasmith662@gmail.com>
2020-02-15 13:55:39 -0600

    Add basic code for backend server with dummy route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const express = require('express');
 
 const app = express(); // invoke express -> store express app in app variable
 
+// import routes
+const authRouter = require('./routes/auth');
+
+// middleware
+app.use('/api', authRouter);
+
 const port = process.env.port || 8000; // .env file port # or 8000
 // start server/listen on port
 app.listen(port, () => {
