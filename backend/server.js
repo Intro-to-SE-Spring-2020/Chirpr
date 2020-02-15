632e273dc6629016540efe976abc619afafe06d3
JustinASmith <justinasmith662@gmail.com>
2020-02-15 14:15:20 -0600

    Add express middleware and connect backend to mongodb atlas database

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,45 @@
 // Entry-point of backend
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express(); // invoke express -> store express app in app variable
+
+// connect mongoose to MongoDB Atlas
+mongoose.connect(process.env.ATLAS_URI, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    // handle errors on initial connection
+    .catch(err => console.error('DB CONNECTION ERROR: ', err));
+
+// handle errors after connection is established
+mongoose.connection.on('error', err => {
+    console.error(err);
+});
 
 // import routes
 const authRouter = require('./routes/auth');
 
 // middleware
+// morgan for better development experience
+app.use(morgan('dev'));
+app.use(express.json());
+// app.use(cors()); // allows all origins
+if ((process.env.NODE_ENV == 'development')) {
+    app.use(cors({ origin: 'http://localhost:3000' }));
+}
+
+// router middleware
 app.use('/api', authRouter);
 
-const port = process.env.port || 8000; // .env file port # or 8000
+const PORT = process.env.port || 8000; // .env file port # or 8000
 // start server/listen on port
-app.listen(port, () => {
+app.listen(PORT, () => {
     // callback function on success
-    console.log(`API is running on port ${port}`);
+    console.log(`API is running on port ${PORT}`);
 });
