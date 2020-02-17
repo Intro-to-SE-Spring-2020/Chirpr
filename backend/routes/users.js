3240d0995b3fe46a8374132418e0c79b2ce9f279
JustinASmith <justinasmith662@gmail.com>
2020-02-17 12:16:36 -0600

    Add users route file for future user profile routes.

---
+++ backend/routes/users.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+// import controllers
+
+// import validators
+
+// import middleware
+
+// @route   UPDATE api/user/profile/:id
+// @desc    Update user profile
+// @access  Private
+// router.update('/user/profile/:id',
+//     userProfileValidator,
+//     runValidation,
+//     auth,
+//     updateProfile
+//     );
