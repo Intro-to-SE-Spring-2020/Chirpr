2e3e78d5336741be4c4e04d1c69358acfed4b2d3
JustinASmith <justinasmith662@gmail.com>
2020-02-20 14:16:54 -0600

    Compress user signup and sigin validator functions into one.
    Add first_name and last_name to userProfileValidator after doing previous statement

--- backend/validators/auth.js
+++ backend/validators/auth.js
@@ -1,16 +1,6 @@
 const { check } = require('express-validator');
 
-exports.userSignupValidator = [
-    check('first_name')
-        .not()
-        .isEmpty()
-        .withMessage('First name is required'),
-
-    check('last_name')
-        .not()
-        .isEmpty()
-        .withMessage('Last name is required'),
-
+exports.userAuthValidator = [
     check('email')
         .isEmail()
         .withMessage('Must be a valid email address'),
@@ -19,14 +9,3 @@ exports.userSignupValidator = [
         .isLength({min: 6})
         .withMessage('Password must be at least 6 characters long')
 ];
-
-exports.userSigninValidator = [
-    check('email')
-        .isEmail()
-        .withMessage('Must be a valid email address'),
-        
-    check('password')
-        .not()
-        .isEmpty()
-        .withMessage('Password is required')
-]
