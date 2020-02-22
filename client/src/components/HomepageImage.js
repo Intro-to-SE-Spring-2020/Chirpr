7ab7b36e2f4380655b52904caff05064198be37f
pranmar123 <pranmar123@gmail.com>
2020-02-22 01:39:32 -0600

    Add landing page v1

---
+++ client/src/components/HomepageImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function HomepageImage() {
+  const url = 'https://i.ibb.co/rdHPc04/chirpr-removebg-preview.png';
+  return (
+    <img src={url} style={{width: 150, height: 150}} alt='Chirpr' />
+  );
+}
+
+export default HomepageImage;
