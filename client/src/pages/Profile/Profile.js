33d2c4b42966ece26cf3928774b7d5eb03ab80f3
pranmar123 <pranmar123@gmail.com>
2020-02-23 16:18:20 -0600

    Comment out profile page since it is still a WIP

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react'
-import Card from '../../components/profileCard/profileCard.js';
-import {Container, Row, Col } from "react-bootstrap";
+// import React, { Component } from 'react'
+// import Card from '../../components/profileCard/profileCard.js';
+// import {Container, Row, Col } from "react-bootstrap";
 
-function Profile(props){
-    const [bio, setBio] = useState("") //initial state of bio is empty
-    function getProfileData(){
-        axios({
-            method: 'get'
-            url: 'http:'
-            data: {
+// function Profile(props){
+//     const [bio, setBio] = useState("") //initial state of bio is empty
+//     function getProfileData(){
+//         axios({
+//             method: 'get'
+//             url: 'http:'
+//             data: {
 
                 
-            }
-        })
+//             }
+//         })
 
 
-    }
-    return (
-        <Container>
-        <Row>
-            <Col> </Col>
-            <Col xs={6}>
-                <Card />
-                </Col>
-            <Col> </Col>
-        </Row>
-    </Container>
-    )
+//     }
+//     return (
+//         <Container>
+//         <Row>
+//             <Col> </Col>
+//             <Col xs={6}>
+//                 <Card />
+//                 </Col>
+//             <Col> </Col>
+//         </Row>
+//     </Container>
+//     )
 
-}
+// }
 
 
-export default Profile
+// export default Profile
 
