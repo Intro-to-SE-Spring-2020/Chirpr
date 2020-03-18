7cb60fa41c4192cb4ded7c83b0c9890112558dea
JustinASmith <justinasmith662@gmail.com>
2020-03-17 21:29:07 -0500

    Add input field for creating a new chirp.
    User can type new chirp in field and click share to add new chirp.

---
+++ client/src/components/ChirpInputForm/ChirpInputForm.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {
+  Card,
+  FormControl,
+  Button,
+  OverlayTrigger,
+  Tooltip
+} from 'react-bootstrap'
+
+import './ChirpInputForm.scss'
+
+const ChirpInputForm = (props) => {
+  return (
+    <>
+      <Card className={["shadow", props.className]}>
+        <Card.Body >
+          <div className="input-wrapper">
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip id="tooltip-top">
+                Most recent chirps will load after sharing
+              </Tooltip>
+            }
+          >
+            <FormControl
+              placeholder="Say something..."
+              className="input"
+              onChange={e => props.setChirpData(e.target.value)}
+              value={props.chirpData}
+            />
+          </OverlayTrigger>
+            <Button onClick={props.shareChirp} className="button" variant="primary">Share</Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </>
+  )
+}
+
+export default ChirpInputForm
