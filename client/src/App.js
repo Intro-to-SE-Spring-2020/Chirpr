c56d67e43445857abb4c74232502a83c2543ebd0
JustinASmith <justinasmith662@gmail.com>
2020-03-21 15:16:59 -0500

    Remove old auth utility functions and implement new useAuthStatus hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route, Switch, withRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
@@ -18,38 +18,29 @@ import Profile from './pages/Profile/Profile'
 
 import useAuthStatus from './lib/hooks/useAuthStatus'
 
-import { isAuthed } from './lib/api/auth'
-import { logout } from '../src/lib/api/auth'
-
 function App (props) {
-  const [authed, setAuthed] = React.useState(false)
-  const isAuth = useAuthStatus();
+  const [logout, setLogout] = useState(false);
+  const isAuthed = useAuthStatus(logout);
   
   const handleLogout = () => {
-    logout()
-    if (!isAuthed()) {
-        setAuthed(false)
-        props.history.push('/')
-    } else {
-        setAuthed(isAuthed())
+    setLogout(true)
+    if (!isAuthed) {
+      setLogout(false)
+      props.history.push('/login')
     }
   }
 
-  useEffect(() => {
-    setAuthed(isAuthed())
-  }, [window.location.href])
-
   return (
       <div data-testid='app' className='App'>
-        <Navigation status={authed} handleLogout={handleLogout} />
+        <Navigation status={isAuthed} handleLogout={handleLogout} />
         <Switch>
           {/* put exact so that the component is only rendered when http://localhost/ */}
+          <PrivateRoute path='/create-profile' isAuthed={isAuthed} exact component={() => <AuthPage register={false} login={false} createProfile />} />
+          <PrivateRoute path="/Feed" isAuthed={isAuthed} exact component={Feed}/>
           <Route exact path='/' component={Landing} />
           <Route path='/register' exact component={() => <AuthPage register login={false} createProfile={false} />} />
           <Route path='/login' exact component={() => <AuthPage register={false} login createProfile={false} />} />
-          <PrivateRoute path='/create-profile' isAuthed={isAuth} exact component={() => <AuthPage register={false} login={false} createProfile />} />
           <Route path='/profile' exact component={() => <Profile />} />
-          <PrivateRoute path="/Feed" isAuthed={isAuth} exact component={Feed}/>
           {/* Add all pages above the error page! -KRW */}
           <Route path='*' component={ErrorPage} />
         </Switch>
