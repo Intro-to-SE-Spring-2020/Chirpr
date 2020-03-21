5c6956daf132eb2af662d322239c91de1244f641
JustinASmith <justinasmith662@gmail.com>
2020-03-21 15:03:27 -0500

    Change default state of isAuthed to false and handle logout

--- client/src/lib/hooks/useAuthStatus.js
+++ client/src/lib/hooks/useAuthStatus.js
@@ -1,17 +1,20 @@
 import { useState, useEffect } from 'react'
 import { withCookies, useCookies } from 'react-cookie'
 
-export default function useAuthStatus() {
-    const [isAuthed, setIsAuthed] = useState(null);
-    const [cookies, setCookies] = useCookies();
+export default function useAuthStatus(logout) {
+    const [isAuthed, setIsAuthed] = useState(false);
+    const [cookies, setCookies, removeCookies] = useCookies();
 
     useEffect(() => {
-        if (cookies["x-auth-token"]) {
-            setIsAuthed(true)
+        if (!logout) {
+            if (cookies["x-auth-token"]) {
+                setIsAuthed(true)
+            }
         } else {
+            removeCookies('x-auth-token')
             setIsAuthed(false)
         }
-    }, [cookies["x-auth-token"]]);
+    }, [cookies["x-auth-token"], logout]);
     
     return isAuthed;
 }
