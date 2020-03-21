262a3a11fe24f7d5915963b621976f8f1cf8c134
JustinASmith <justinasmith662@gmail.com>
2020-03-21 15:12:43 -0500

    Fix previous coding error for useAuthStatus hook.
    This properly handles all known casses with private routing.

--- client/src/lib/hooks/useAuthStatus.js
+++ client/src/lib/hooks/useAuthStatus.js
@@ -2,18 +2,23 @@ import { useState, useEffect } from 'react'
 import { withCookies, useCookies } from 'react-cookie'
 
 export default function useAuthStatus(logout) {
-    const [isAuthed, setIsAuthed] = useState(false);
+    const [isAuthed, setIsAuthed] = useState(null);
     const [cookies, setCookies, removeCookies] = useCookies();
 
     useEffect(() => {
-        if (!logout) {
-            if (cookies["x-auth-token"]) {
-                setIsAuthed(true)
+        async function handleEffect() {
+            if (!logout) {
+                if (cookies["x-auth-token"]) {
+                    setIsAuthed(true)
+                } else {
+                    setIsAuthed(false)
+                }
+            } else {
+                removeCookies('x-auth-token')
+                setIsAuthed(false)
             }
-        } else {
-            removeCookies('x-auth-token')
-            setIsAuthed(false)
         }
+        handleEffect()
     }, [cookies["x-auth-token"], logout]);
     
     return isAuthed;
