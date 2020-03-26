e2cb4b1410fd3b13d65a30897ff8c5d995010349
JustinASmith <justinasmith662@gmail.com>
2020-03-26 12:32:14 -0500

    Add network reducer to root reducer with 'network' as state key

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,7 +2,9 @@ import { combineReducers } from 'redux';
 
 // import reducers
 import authReducer from './authReducer';
+import networkReducer from './networkReducer';
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    network: networkReducer
 });
