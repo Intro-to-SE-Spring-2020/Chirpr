90d00b502a0f9bc763e02cc3d6efeaf1e57db4bf
JustinASmith <justinasmith662@gmail.com>
2020-03-26 12:25:56 -0500

    Convert login component to use redux actions

--- client/src/components/LoginAndReg/Login.js
+++ client/src/components/LoginAndReg/Login.js
@@ -1,17 +1,14 @@
 import React, { useState } from 'react'
 import { FormGroup, FormControl, FormLabel } from 'react-bootstrap'
 import { withRouter } from 'react-router-dom'
-import { withCookies, useCookies } from 'react-cookie'
-import { compose } from 'recompose'
 import { connect } from 'react-redux'
 
-// import action creator
-import { login } from '../../actions';
+import { login, logout } from '../../actions/'
+
 import LoaderButton from '../Buttons/LoaderButton'
 import AlertMessage from '../AlertMessage/AlertMessage'
 
 const Login = (props) => {
-  const [cookies, setCookie] = useCookies();
   const [isLoading, setIsLoading] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -23,53 +20,10 @@ const Login = (props) => {
     return email.length > 0 && password.length > 5
   }
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault()
 
     props.login(email, password);
-
-    // setIsLoading(true)
-    // try {
-    //   // call api
-    //   const res = await axios({
-    //     method: 'post',
-    //     url: 'http://localhost:8000/api/signin',
-    //     data: {
-    //       email,
-    //       password
-    //     }
-    //   });
-      
-    //   if (res.status === 200) {
-    //     const expireDate = new Date()
-    //     expireDate.setMinutes(expireDate.getMinutes() + 119)
-    //     setCookie('x-auth-token', res.data.token, { path: '/', expires:  expireDate })
-  
-    //     if (res.data.hasProfile) {
-    //       // parse info here and create dummy profile info
-    //       props.history.push('/feed')
-    //     } else {
-    //       props.history.push('/create-profile')
-    //     }
-    //   }
-    //   else {
-    //     setEmail('')
-    //     setPassword('')
-    //     setMsg(`Status: ${res.status}. Could not sign in! Try refreshing page.`)
-    //     setError(true)
-    //   }
-    // } catch (err) {
-    //   if (err.response.status === 400) {
-    //     setMsg(`${err.response.data.error}.`)
-    //     setError(true)
-    //     setIsLoading(false)
-    //   }
-    //   else {
-    //     setMsg(`${err.message}. Could not sign in! Try refreshing page.`)
-    //     setError(true)
-    //     setIsLoading(false)
-    //   }
-    // }
   }
   
   return (
@@ -109,7 +63,7 @@ const Login = (props) => {
 }
 
 const mapStateToProps = (state) => {
-  return { auth: state.auth };
+  return { auth: state.auth }
 }
 
-export default withRouter(connect(mapStateToProps, { login })(Login))
+export default withRouter(connect(mapStateToProps, { login, logout })(Login))
