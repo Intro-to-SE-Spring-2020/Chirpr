0a18352e02abc99aac8d8100eecd9d5213a6d819
JustinASmith <justinasmith662@gmail.com>
2020-03-26 12:30:07 -0500

    Handle actions in auth reducer properly

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -3,23 +3,35 @@ import {
  LOGIN,
  LOGOUT,
  REGISTER,
- GET_USER_PROFILE
+ GET_USER_PROFILE,
+ REDIRECT_STATUS
 } from "../actions/types"
 
 // initial state
 const initialState = {
-    token: localStorage.getItem('token'),
-    expiry: localStorage.getItem('expiry'),
-    profile: null
+    token: null,
+    expiry: null,
+    profile: null,
+    redirect: "/"
 }
 
 // authentication reducer
 export default (state = initialState, action) => {
     switch (action.type) {
         case LOGIN:
-            return { ...state, profile: action.payload }
+            return {
+                ...state,
+                token: action.payload.data.token,
+                profile: action.payload.data.profile,
+                expiry: action.payload.expiry
+            }
+        case LOGOUT:
+            // action.payload.history.push('/login') // PICk UP HERE!
+            return { ...initialState }
         case GET_USER_PROFILE:
             return { ...state, profile: action.payload }
+        case REDIRECT_STATUS:
+            return { ...state, redirect: action.payload }
         default:
             return state;
     }
