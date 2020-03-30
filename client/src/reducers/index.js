8df1fde644a3195cc248f597af851410b00161fb
JustinASmith <justinasmith662@gmail.com>
2020-03-29 22:24:53 -0500

    Combine chirps reducer into rute reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,8 +3,10 @@ import { combineReducers } from 'redux';
 // import reducers
 import authReducer from './authReducer';
 import networkReducer from './networkReducer';
+import chirpReducer from './chirpReducer'
 
 export default combineReducers({
     auth: authReducer,
-    network: networkReducer
+    network: networkReducer,
+    chirps: chirpReducer
 });
