021a663869496f57a8f469fec268697c91759cd7
Keith Wilcox <keithwilcoxr@gmail.com>
2020-04-13 00:17:27 -0500

    Added second test

--- client/src/components/Chirp/__tests__/like.js
+++ client/src/components/Chirp/__tests__/like.js
@@ -19,33 +19,59 @@ describe('Chirp React-Redux Component', () => {
       mockAxios.mockClear()
     })
 
-    const setup = (state = null) => {
+    const unlikeSetup = (state = null) => {
         
         const data = { name:'Keith', username:'@Rwkeith', content:'TEST TWEET', likes:10, retweets:3, time:'', isOwned:true, isLiked:false, isReChirped:false, _id:1 }
         // render component with custom render function
         const component = renderWithRedux(<Chirp data={data}/>, { initialState: state })
         // destructure attributes from component
         const { getByTestId, store  } = component
-        const likeButton = getByTestId('unlike-button')
+        const unlikeButton = getByTestId('unlike-button')
+        const clickUnlike = () => fireEvent.click(unlikeButton)
+        return {
+          ...component,
+          store, // mocked redux store
+          unlikeButton, // unlike button
+          clickUnlike // handler for submit button click
+        }
+      }
+
+    const likeSetup = (state = null) => {
+        const data = { name:'Keith', username:'@Rwkeith', content:'TEST TWEET', likes:10, retweets:3, time:'', isOwned:true, isLiked:true, isReChirped:false, _id:1 }
+        // render component with custom render function
+        const component = renderWithRedux(<Chirp data={data} />, { initialState: state })
+        // destructure attributes from component
+        const { getByTestId, store  } = component
+        const likeButton = getByTestId('like-button')
         const clickLike = () => fireEvent.click(likeButton)
         return {
           ...component,
           store, // mocked redux store
-          likeButton, // submit button on form
+          likeButton,
           clickLike // handler for submit button click
         }
-      }
+    }
     
       // TEST CASE 1: Clicking the like button gives correct feedback
-      test('user clicks like button and a like is added', async () => {
+      test('The chirp has been liked', async () => {
         // setup Chirp component
-        const component = setup()
+        const unlikeComponent = unlikeSetup()
         // destructure attributes from component
         const {
-          store,
+          unlikeButton,
+          clickUnlike
+        } = unlikeComponent
+
+        expect(unlikeButton).toBeVisible()
+        
+      })
+
+      test("The chirp has not been liked", async () => {
+        const likeComponent = likeSetup()
+
+        const {
           likeButton,
-          clickLike
-        } = component
+        } = likeComponent
 
         expect(likeButton).toBeVisible()
       })
